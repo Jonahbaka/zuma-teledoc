866edb138377ecfae41cb6d4c9b979307f0665c9
Jonah Baka <Jonahbaka00@gmail.com>
2026-02-05 12:31:35 -0800

    fix: Add port 3001 to Socket.io CORS and improve debugging

--- server/services/socketService.js
+++ server/services/socketService.js
@@ -24,7 +24,9 @@ const initializeSocket = (httpServer) => {
       origin: [
         appUrl,
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:3001',
         'https://doctarx.com',
         'https://www.doctarx.com'
       ],
