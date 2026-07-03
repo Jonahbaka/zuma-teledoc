4190de9c8d17cf52f17f18e6e5fa50934649f7ef
Jonah Baka <Jonahbaka00@gmail.com>
2026-07-03 13:11:24 -0700

    Fix NG presentation video playback interaction

--- app/ng-presentation/route.js
+++ app/ng-presentation/route.js
@@ -10,7 +10,11 @@ export async function GET() {
   return new Response(html, {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
-      'Cache-Control': 'public, max-age=300, must-revalidate'
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+      'Surrogate-Control': 'no-store',
+      'CDN-Cache-Control': 'no-store'
     }
   });
 }
